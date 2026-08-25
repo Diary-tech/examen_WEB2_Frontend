ef6bc9dfd7117acd2bc7eb92ba8675d3f8e2012f
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-08-25 18:44:25 +0300

    feat: adding the rest of the logic and also editing the main.jsx for the react-router-dom

--- examenHub_frontend/src/pages/Login.jsx
+++ examenHub_frontend/src/pages/Login.jsx
@@ -1,20 +1,35 @@
 import { useState } from 'react';
+import{useNavigate} from 'react-router-dom';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:3000/api/login', {
+    const response = await fetch('http://localhost:3000/api/auth/login', {
         method: 'POST',
         headers: {'Content-Type': 'application/json',},
         body: JSON.stringify({ email, password }),
     });
 
     const data = await response.json();
-  }
+    console.log(data);
+    if (response.ok) {
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('user', JSON.stringify(data.user));
+
+      if (data.user.role === 'admin') {
+        navigate('/admin');
+      } else {
+        navigate('/student');
+      }
+    } else {
+      alert(data.message);
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
