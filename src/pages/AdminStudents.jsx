f0c28fe87ebd14e2eba78997b582b30ffa2a1115
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-08-26 14:59:32 +0300

    feat: creating the adminStudent file

---
+++ src/pages/AdminStudents.jsx
@@ -0,0 +1,10 @@
+import {useState, useEffect} from 'react';
+
+export default function AdminStudents() {
+    return(
+        <div className="page">
+            <h1>Manage Students</h1>
+            <p>This is the student management page.</p>
+        </div>
+    );
+}
