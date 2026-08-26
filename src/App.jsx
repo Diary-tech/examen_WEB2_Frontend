51f877b686400f581208a30a41577d5e446922aa
adrianoramilison <mandaramilison19@gmail.com>
2026-08-26 17:32:19 +0300

    feat(admin): add exam management interface

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,38 @@ import Login from './pages/Login.jsx';
 import AdminLayout from './components/AdminLayout.jsx';
 import AdminHome from './pages/AdminHome.jsx';
 import AdminStudents from './pages/AdminStudents.jsx';
+import AdminExams from './pages/AdminExams.jsx';
 import StudentHome from './pages/StudentHome.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
-import AdminCourses from './pages/AdminCourses.jsx';
 import './App.css';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />} />
-      <Route path="/login" element={<Login />} />
-
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute role="admin">
-            <AdminLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<AdminHome />} />
-        <Route path="students" element={<AdminStudents />} />
-        <Route path="courses" element={<AdminCourses />} />
-      </Route>
-
-      <Route
-        path="/student"
-        element={
-          <ProtectedRoute role="student">
-            <StudentHome />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+                path="/admin"
+                element={
+                    <ProtectedRoute role="admin">
+                        <AdminLayout />
+                    </ProtectedRoute>
+                }
+            >
+                <Route index element={<AdminHome />} />
+                <Route path="students" element={<AdminStudents />} />
+                <Route path="exams" element={<AdminExams />} />
+            </Route>
+            <Route
+                path="/student"
+                element={
+                    <ProtectedRoute role="student">
+                        <StudentHome />
+                    </ProtectedRoute>
+                }
+            />
+        </Routes>
+    );
 }
 
 export default App;
