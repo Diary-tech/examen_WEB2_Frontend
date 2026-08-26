8076a6ef9d8e1bd8305621178ba8d0a222147e19
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-08-26 14:49:30 +0300

    chore: updating the css style

--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { get } from '../api/client.js';
+import './AdminHome.css';
 
 export default function AdminHome() {
   const [counts, setCounts] = useState({
