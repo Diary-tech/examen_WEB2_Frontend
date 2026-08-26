2ce1d1f572a5def82013512cd484a4b0ac66d056
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-08-26 13:55:25 +0300

    feat: creating the html content

--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -1,4 +1,29 @@
 
 export default function AdminHome() {
-  return (<h1>Espace Admin</h1>);
+  return (
+  <> 
+  <h1>Admin Dashboard</h1> 
+  <div>
+        <div >
+          <h2>{counts.students ?? '...'}</h2>
+          <p>Students</p>
+        </div>
+        <div>
+          <h2>{counts.courses ?? '...'}</h2>
+          <p>Courses</p>
+        </div>
+        <div>
+          <h2>{counts.exams ?? '...'}</h2>
+          <p>Exams</p>
+        </div>
+      </div>
+
+      <h2>Quick Links</h2>
+      <ul>
+        <li><Link to="/admin/students">Manage Students</Link></li>
+        <li><Link to="/admin/courses">Manage Courses</Link></li>
+        <li><Link to="/admin/exams">Manage Exams</Link></li>
+      </ul>
+  </>
+  );
 }
